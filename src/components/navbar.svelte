1a3cd8d1a0254f6adc9c2dc0bf79e2f0f69c0fc1
Quixelation <58915890+Quixelation@users.noreply.github.com>
2021-04-09 01:49:40 +0200

    fix wrong link

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -54,7 +54,7 @@
     <li><a href="#twitch"><span>Twitch</span></a></li>
     <li><a href="#youtube"><span>Youtube</span></a></li>
     <li>
-      <a href="/https://www.instagram.com/scaryMikrowelle/"
+      <a href="https://www.instagram.com/scaryMikrowelle/"
         ><span>Instagram</span></a
       >
     </li>
