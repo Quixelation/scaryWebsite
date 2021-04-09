3a6fafdee50c45746255cf41470c87b6c87d161c
scaryMikrowelle <clemens@clemensgreulich.de>
2021-04-09 10:42:25 +0200

    Update impressum.svelte

--- src/pages/impressum.svelte
+++ src/pages/impressum.svelte
@@ -1,1 +1,34 @@
-<table />
+<table class="tg">
+<thead>
+    <tr>
+        <th class="tg-km2t">Name</th>
+        <th class="tg-zv4m">Clemens Jonathan Greulich</th>
+    </tr>
+</thead>
+<tbody>
+    <tr>
+        <td class="tg-km2t">Kontakt</td>
+        <td class="tg-zv4m">Telefon: +4915123455552</td>
+    </tr>
+    <tr>
+        <td class="tg-zv4m"></td>
+        <td class="tg-zv4m">Email: clemens@clemensgreulich.de</td>
+    </tr>
+    <tr>
+        <td class="tg-km2t">Anschrift</td>
+        <td class="tg-zv4m">Marderweg 7</td>
+    </tr>
+    <tr>
+        <td class="tg-zv4m"></td>
+        <td class="tg-zv4m">07749</td>
+    </tr>
+    <tr>
+        <td class="tg-zv4m"></td>
+        <td class="tg-zv4m">Jena</td>
+    </tr>
+    <tr>
+        <td class="tg-zv4m"></td>
+        <td class="tg-zv4m">Deutschland</td>
+    </tr>
+</tbody>
+</table>
