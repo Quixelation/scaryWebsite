8ecac05c95ca0dfd469f99410fd8f7f85f4fbf80
scaryMikrowelle <clemens@clemensgreulich.de>
2021-04-09 11:17:06 +0200

    impressum die 30te

--- src/pages/impressum.svelte
+++ src/pages/impressum.svelte
@@ -4,14 +4,27 @@
 
 <Navbar />
 
+<style type="text/css">
+    .tg  {border-collapse:collapse;border-spacing:0;}
+    .tg td{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
+      overflow:hidden;padding:10px 5px;word-break:normal;}
+    .tg th{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
+      font-weight:normal;overflow:hidden;padding:10px 5px;word-break:normal;}
+    .tg .tg-km2t{border-color:#ffffff;font-weight:bold;text-align:left;vertical-align:top}
+    .tg .tg-zv4m{border-color:#ffffff;text-align:left;vertical-align:top}
+    .tg .tg-0lax{text-align:left;vertical-align:top}
+</style>
 <table class="tg">
-<tbody>
+<thead>
     <tr>
         <th class="tg-km2t">Name</th>
         <th class="tg-zv4m">Clemens Jonathan Greulich</th>
     </tr>
+</thead>
+<tbody>
     <tr>
-        <td>&nbsp;</td>
+        <td class="tg-0lax"></td>
+        <td class="tg-0lax"></td>
     </tr>
     <tr>
         <td class="tg-km2t">Kontakt</td>
@@ -22,7 +35,8 @@
         <td class="tg-zv4m">clemens@clemensgreulich.de</td>
     </tr>
     <tr>
-        <td>&nbsp;</td>
+        <td class="tg-0lax"></td>
+        <td class="tg-0lax"></td>
     </tr>
     <tr>
         <td class="tg-km2t">Anschrift</td>
