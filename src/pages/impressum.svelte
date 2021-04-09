880e4eefbc4886295d5da046c8d920e7e8cf58af
scaryMikrowelle <clemens@clemensgreulich.de>
2021-04-09 11:23:13 +0200

    Update impressum.svelte
    weiße Tabellenränder sollten weg sein

--- src/pages/impressum.svelte
+++ src/pages/impressum.svelte
@@ -5,14 +5,14 @@
 <Navbar />
 
 <style type="text/css">
-    .tg  {border-collapse:collapse;border-spacing:0;}
-    .tg td{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
+    .tg  {border:none;border-collapse:collapse;border-spacing:0;}
+    .tg td{border-style:solid;border-width:0px;font-family:Arial, sans-serif;font-size:14px;overflow:hidden;
+      padding:10px 5px;word-break:normal;}
+    .tg th{border-style:solid;border-width:0px;font-family:Arial, sans-serif;font-size:14px;font-weight:normal;
       overflow:hidden;padding:10px 5px;word-break:normal;}
-    .tg th{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
-      font-weight:normal;overflow:hidden;padding:10px 5px;word-break:normal;}
     .tg .tg-km2t{border-color:#ffffff;font-weight:bold;text-align:left;vertical-align:top}
     .tg .tg-zv4m{border-color:#ffffff;text-align:left;vertical-align:top}
-    .tg .tg-0lax{text-align:left;vertical-align:top}
+    .tg .tg-0pky{border-color:inherit;text-align:left;vertical-align:top}
 </style>
 <table class="tg">
 <thead>
@@ -23,20 +23,20 @@
 </thead>
 <tbody>
     <tr>
-        <td class="tg-0lax"></td>
-        <td class="tg-0lax"></td>
+        <td class="tg-0pky"></td>
+        <td class="tg-0pky"></td>
     </tr>
     <tr>
         <td class="tg-km2t">Kontakt</td>
-        <td class="tg-zv4m">+4915123455552</td>
+        <td class="tg-zv4m">Telefon: +4915123455552</td>
     </tr>
     <tr>
         <td class="tg-zv4m"></td>
-        <td class="tg-zv4m">clemens@clemensgreulich.de</td>
+        <td class="tg-zv4m">Email: clemens@clemensgreulich.de</td>
     </tr>
     <tr>
-        <td class="tg-0lax"></td>
-        <td class="tg-0lax"></td>
+        <td class="tg-0pky"></td>
+        <td class="tg-0pky"></td>
     </tr>
     <tr>
         <td class="tg-km2t">Anschrift</td>
